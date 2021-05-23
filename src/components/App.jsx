abedffe765103ccc13bf970cdf5ded3805cea7bb
Nikolay Rademacher <hrrdx33@gmail.com>
2021-05-23 21:09:21 +0200

    chore: add font faces

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => (
-  <h1 className="text-brand-blue grid place-items-center min-h-screen text-8xl font-light">
+  <h1 className="text-brand-blue grid place-items-center min-h-screen text-8xl">
     Hello, world.
   </h1>
 );
